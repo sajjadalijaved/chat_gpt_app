3ad23006b7785cc163c324648d4979b90a667180
sajjadalijaved <sajjadali11755@gmail.com>
2023-08-09 20:59:05 +0500

    save

--- lib/constans/api_constant.dart
+++ lib/constans/api_constant.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
